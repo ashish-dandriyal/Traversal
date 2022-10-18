bfea7d98a81bb795f689bdd344e67883a276e331
Ashish Dandriyal <ashish.dandriyal@irdeto.com>
2022-10-18 08:48:47 +0530

    minor updates related to varible name and assert in logic

--- src/main/java/Traverse2DArray.java
+++ src/main/java/Traverse2DArray.java
@@ -19,13 +19,12 @@ public class Traverse2DArray {
     public static void main(String args[]) {
         int row = 4;
         int col = 4;
-        assert col < 2 : "wrong value";
         int arr[][] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
         String spiral = spiralPrint(row, col, arr);
         System.out.println(spiral);
     }
 
-    public static String spiralPrint(int row, int column, int a[][]) {
+    public static String spiralPrint(int row, int column, int arr[][]) {
         List<String> orderedItem = new ArrayList();
 
         /*
@@ -37,30 +36,34 @@ public class Traverse2DArray {
          */
         int itr, top = 0, left = 0, bottom = row, right = column;
 
+        assert row > 0 : "Row must be greater than zero.";
+        assert column > 0 : "Column must be greater than zero.";
+        assert arr != null : "2D array must not be null.";
+
         // condition to make sure that there are elements in array to be added
         while (top < bottom && left < right) {
             // adds the first row from the remaining rows
             for (itr = left; itr < right; ++itr) {
-                orderedItem.add(a[top][itr] + "");
+                orderedItem.add(arr[top][itr] + "");
             }
             top++;
 
             // adds the last column from the remaining columns
             for (itr = top; itr < bottom; ++itr) {
-                orderedItem.add(a[itr][right - 1] + "");
+                orderedItem.add(arr[itr][right - 1] + "");
             }
             right--;
             // adds the last row from the remaining rows
             if (top < bottom) {
                 for (itr = right - 1; itr >= left; --itr) {
-                    orderedItem.add(a[bottom - 1][itr] + "");
+                    orderedItem.add(arr[bottom - 1][itr] + "");
                 }
                 bottom--;
             }
             // adds the first column from the remaining columns
             if (left < right) {
                 for (itr = bottom - 1; itr >= top; --itr) {
-                    orderedItem.add(a[itr][left] + "");
+                    orderedItem.add(arr[itr][left] + "");
                 }
                 left++;
             }
--- src/test/java/Traverse2DArrayTest.java
+++ src/test/java/Traverse2DArrayTest.java
@@ -6,17 +6,17 @@ class Traverse2DArrayTest {
 
     @Test
     void verifyForRightInput() {
-        String validateAgainst = "1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10";
+        String expectedSequence = "1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10";
         int arr[][] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
-        String result = Traverse2DArray.spiralPrint(4, 4, arr);
-        assertEquals(result, validateAgainst);
+        String actualSequence = Traverse2DArray.spiralPrint(4, 4, arr);
+        assertEquals(expectedSequence, actualSequence);
     }
 
     @Test
     void verifyForWrongInput() {
-        String validateAgainst = "1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10";
+        String expectedSequence = "1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10";
         int arr[][] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
-        String result = Traverse2DArray.spiralPrint(2, 2, arr);
-        assertNotEquals(result, validateAgainst);
+        String actualSequence = Traverse2DArray.spiralPrint(2, 2, arr);
+        assertNotEquals(expectedSequence, actualSequence);
     }
 }
